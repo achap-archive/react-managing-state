600fe2fa51af0e8c2395811ca5953099a2cc5a62
Ashlyn Chapman <apchapma@ncsu.edu>
2021-06-22 20:04:04 -0400

    add routing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { BrowserRouter } from "react-router-dom"
 import ErrorBoundary from "./ErrorBoundary"
 
 ReactDOM.render(
     <ErrorBoundary>
-      <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </ErrorBoundary>,
     document.getElementById("root")
   );
